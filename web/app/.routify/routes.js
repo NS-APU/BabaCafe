432eb356a96504ee866877d1b8594e9be9c5e5da
keis8221 <kei_s8221@icloud.com>
2022-12-08 13:00:07 +0900

    refactor: code format

--- web/app/.routify/routes.js
+++ web/app/.routify/routes.js
@@ -1,50 +1,50 @@
-
 /**
  * @roxi/routify 2.18.8
- * File generated Wed Dec 07 2022 15:34:28 GMT+0900 (日本標準時)
+ * File generated Thu Dec 08 2022 12:59:14 GMT+0900 (日本標準時)
  */
 
-export const __version = "2.18.8"
-export const __timestamp = "2022-12-07T06:34:28.210Z"
+export const __version = "2.18.8";
+export const __timestamp = "2022-12-08T03:59:14.114Z";
 
 //buildRoutes
-import { buildClientTree } from "@roxi/routify/runtime/buildRoutes"
+import { buildClientTree } from "@roxi/routify/runtime/buildRoutes";
 
 //imports
 
-
 //options
-export const options = {}
+export const options = {};
 
 //tree
 export const _tree = {
-  "root": true,
-  "children": [
+  root: true,
+  children: [
     {
-      "isIndex": true,
-      "isPage": true,
-      "path": "/index",
-      "id": "_index",
-      "component": () => import('../src/pages/index.svelte').then(m => m.default)
+      isIndex: true,
+      isPage: true,
+      path: "/index",
+      id: "_index",
+      component: () =>
+        import("../src/pages/index.svelte").then((m) => m.default),
     },
     {
-      "isDir": true,
-      "ext": "",
-      "children": [
+      isDir: true,
+      ext: "",
+      children: [
         {
-          "isIndex": true,
-          "isPage": true,
-          "path": "/user/index",
-          "id": "_user_index",
-          "component": () => import('../src/pages/user/index.svelte').then(m => m.default)
-        }
+          isIndex: true,
+          isPage: true,
+          path: "/reservation/index",
+          id: "_reservation_index",
+          component: () =>
+            import("../src/pages/reservation/index.svelte").then(
+              (m) => m.default
+            ),
+        },
       ],
-      "path": "/user"
-    }
+      path: "/reservation",
+    },
   ],
-  "path": "/"
-}
+  path: "/",
+};
 
-
-export const {tree, routes} = buildClientTree(_tree)
-
+export const { tree, routes } = buildClientTree(_tree);
