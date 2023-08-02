8684aae2bedf1fd859c4fe2ca342914c575b778e
KitajimaYoshiki <yoshiki_kitajima@A080791400.rfg.rfgad.ricoh.com>
2023-08-02 10:19:39 +0900

    accordionの実装

---
+++ web/app/src/pages/logistics/setting/_components/LogisticsRouteAccordion.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Button from '@smui/button';
+  import IconButton from '@smui/icon-button';
+  import LogisticsVehicleAccordion from './LogisticsVehicleAccordion.svelte';
+
+  export let routeName: string;
+</script>
+
+<div class="">
+  <details class="mb-2 mt-2 outline outline-1 outline-offset-2 outline-[#000000]">
+    <!-- TODO -->
+    <summary class="bg-[#f4f4f4] px-3 py-2 text-lg text-[#494949]">
+      ### {routeName} ###
+      <IconButton class="material-icons inline-block align-middle">edit</IconButton>
+      <IconButton class="material-icons inline-block align-middle">delete</IconButton>
+    </summary>
+
+    <!-- TODO: roop処理 -->
+    <LogisticsVehicleAccordion vehicleName={'便アコーディオン (1便)'} />
+    <LogisticsVehicleAccordion vehicleName={'便アコーディオン (2便)'} />
+
+    <Button class="mt-2 w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
+      <p class="text-lg font-bold">便追加</p>
+    </Button>
+  </details>
+</div>
--- web/app/src/pages/logistics/setting/_components/LogisticsSettingForm.svelte
+++ web/app/src/pages/logistics/setting/_components/LogisticsSettingForm.svelte
@@ -1,190 +1,84 @@
 <script lang="ts">
+  import { goto } from '@roxi/routify';
   import Button from '@smui/button';
-  import IconButton, { Icon } from '@smui/icon-button';
+  import CircularProgress from '@smui/circular-progress';
+  import IconButton from '@smui/icon-button';
+  import { LogisticsRepository } from '../../../../models/Logistics';
+  import { profile } from '../../../../stores/Account';
+  import { markAsLogoutState } from '../../../../stores/Login';
+  import { addToast } from '../../../../stores/Toast';
+  import LogisticsRouteAccordion from './LogisticsRouteAccordion.svelte';
+
+  let logisticsRepository: LogisticsRepository = new LogisticsRepository();
+
+  // async function fetchLogisticsSetting(): Promise<TLogisticsSetting> {
+  async function fetchLogisticsSetting() {
+    try {
+      return await logisticsRepository.getLogisticsSetting($profile.id);
+    } catch (err) {
+      handleError(err, '物流設定の取得');
+      return [];
+    }
+  }
+
+  function handleError(err, operation) {
+    switch (err.error || err.message) {
+      case 'Bad Request':
+        addToast({
+          message: `${operation}に失敗しました。開発者へお問い合わせください。`,
+          type: 'error',
+        });
+        break;
+      case 'Unauthorized':
+        markAsLogoutState();
+        addToast({
+          message: '認証が切れました。再度ログインしてください。',
+          type: 'error',
+        });
+        $goto('/login');
+        break;
+      default:
+        addToast({
+          message: `${operation}に失敗しました。もう一度時間をおいて再読み込みしてください。`,
+          type: 'error',
+        });
+        break;
+    }
+  }
 </script>
 
-<!-- <div class="grid justify-center"> -->
-<div class="grid">
-  <div class="container">
-    <div class="text-lg">
-      <div class="top_txt">
-        <p class="">集荷・配送方法</p>
-        <!-- TODO: Icon実装 -->
-        <IconButton>
-          <Icon tag="material-icons">edit</Icon>
-        </IconButton>
-      </div>
-      <!-- TODO -->
-      <p class="mt-4">### 集荷・配送方法の設定表示欄 ###</p>
+{#await fetchLogisticsSetting()}
+  <div style="display: flex; justify-content: center">
+    <CircularProgress style=" width: 32px;height: 160px;" indeterminate />
+  </div>
+{:then logisticsSetting}
+  <!-- <div class="grid justify-center"> -->
+  <div class="grid">
+    <div class="container">
+      <div class="text-lg">
+        <div
+          class="relative mt-3 flex justify-between border-l-8 border-solid border-l-primary bg-[#f4f4f4] px-3 py-2 text-lg text-[#494949]"
+        >
+          <p class="flex w-[260px] flex-row flex-wrap items-center text-left text-xl">集荷・配送方法</p>
+          <IconButton class="material-icons inline-block align-middle">edit</IconButton>
+        </div>
+        <!-- TODO -->
+        <!-- <p class="mt-4">### 集荷・配送方法の設定表示欄 ###</p> -->
+        <!-- {#if logisticsSetting.} -->
+        <p class="mt-4" />
 
-      <p class="top_txt mt-8">巡回経路の設定</p>
-      <div class="">
-        <details class="mb-2 mt-2 outline outline-1 outline-offset-2 outline-[#000000]">
-          <!-- TODO -->
-          <summary class="accordion">### 路線アコーディオン 上り ###</summary>
-          <!-- TODO: Icon実装、summaryと同行に表示 -->
-          <IconButton>
-            <Icon tag="material-icons">edit</Icon>
-          </IconButton>
-          <IconButton>
-            <Icon tag="material-icons">delete</Icon>
-          </IconButton>
-          <div class="ml-6">
-            <details class="mb-2 mt-2 outline outline-1 outline-offset-2 outline-[#000000]">
-              <!-- TODO -->
-              <!-- 1便目 -->
-              <summary class="accordion">### 便アコーディオン (1便) ###</summary>
-              <p class="top_txt mt-2">車両</p>
+        <p class="mt-3 border-l-8 border-solid border-l-primary bg-[#f4f4f4] px-3 py-2 text-lg text-[#494949]">
+          巡回経路の設定
+        </p>
 
-              <div class="flex">
-                <div class="mt-2">
-                  <p>衝撃</p>
-                  <p class="mt-2">最大取扱量</p>
-                </div>
-                <div class="ml-5 mt-2">
-                  <!-- TODO -->
-                  <p>### データ取得 ###</p>
-                  <p class="mt-2">### データ取得 ###</p>
-                </div>
-              </div>
-              <p class="top_txt mt-4">時刻表</p>
-              <p class="mt-2">### 表追加 ###</p>
+        <!-- TODO: roop処理 -->
+        <LogisticsRouteAccordion routeName={'路線アコーディオン 上り'} />
+        <LogisticsRouteAccordion routeName={'路線アコーディオン 下り'} />
 
-              <!-- TODO:ボタン配置しただけ -->
-              <Button class="  mr-2 w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便編集</p>
-              </Button>
-
-              <Button class="w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便削除</p>
-              </Button>
-            </details>
-            <!-- 2便目 -->
-            <details class="mb-2 mt-2 outline outline-1 outline-offset-2 outline-[#000000]">
-              <summary class="accordion">### 便アコーディオン (2便) ###</summary>
-              <p class="top_txt mt-2">車両</p>
-
-              <div class="flex">
-                <div class="mt-2">
-                  <p>衝撃</p>
-                  <p class="mt-2">最大取扱量</p>
-                </div>
-                <div class="ml-5 mt-2">
-                  <!-- TODO -->
-                  <p>### データ取得 ###</p>
-                  <p class="mt-2">### データ取得 ###</p>
-                </div>
-              </div>
-              <p class="top_txt mt-4">時刻表</p>
-              <p class="mt-2">### 表追加 ###</p>
-
-              <!-- TODO:ボタン配置しただけ -->
-              <Button class="  mr-2 w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便編集</p>
-              </Button>
-
-              <Button class="w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便削除</p>
-              </Button>
-            </details>
-            <Button class="w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-              <p class="text-lg font-bold">便追加</p>
-            </Button>
-          </div>
-        </details>
-      </div>
-
-      <div class="">
-        <details class="mb-2 mt-2 outline outline-1 outline-offset-2 outline-[#000000]">
-          <!-- TODO -->
-          <summary class="accordion">### 路線アコーディオン 下り ###</summary>
-          <!-- TODO: Icon実装、summaryと同行に表示 -->
-          <IconButton>
-            <Icon tag="material-icons">edit</Icon>
-          </IconButton>
-          <IconButton>
-            <Icon tag="material-icons">delete</Icon>
-          </IconButton>
-          <div class="ml-6">
-            <details class="mb-2 mt-2 outline outline-1 outline-offset-2 outline-[#000000]">
-              <!-- TODO -->
-              <!-- 1便目 -->
-              <summary class="accordion">### 便アコーディオン (1便) ###</summary>
-              <p class="top_txt mt-2">車両</p>
-
-              <div class="flex">
-                <div class="mt-2">
-                  <p>衝撃</p>
-                  <p class="mt-2">最大取扱量</p>
-                </div>
-                <div class="ml-5 mt-2">
-                  <!-- TODO -->
-                  <p>### データ取得 ###</p>
-                  <p class="mt-2">### データ取得 ###</p>
-                </div>
-              </div>
-              <p class="top_txt mt-4">時刻表</p>
-              <p class="mt-2">### 表追加 ###</p>
-
-              <!-- TODO:ボタン配置しただけ -->
-              <Button class="  mr-2 w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便編集</p>
-              </Button>
-
-              <Button class="w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便削除</p>
-              </Button>
-            </details>
-            <!-- 2便目 -->
-            <details class="mb-2 mt-2 outline outline-1 outline-offset-2 outline-[#000000]">
-              <summary class="accordion">### 便アコーディオン (2便) ###</summary>
-              <p class="top_txt mt-2">車両</p>
-
-              <div class="flex">
-                <div class="mt-2">
-                  <p>衝撃</p>
-                  <p class="mt-2">最大取扱量</p>
-                </div>
-                <div class="ml-5 mt-2">
-                  <!-- TODO -->
-                  <p>### データ取得 ###</p>
-                  <p class="mt-2">### データ取得 ###</p>
-                </div>
-              </div>
-              <p class="top_txt mt-4">時刻表</p>
-              <p class="mt-2">### 表追加 ###</p>
-
-              <!-- TODO:ボタン配置しただけ -->
-              <Button class="  mr-2 w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便編集</p>
-              </Button>
-
-              <Button class="w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-                <p class="text-lg font-bold">便削除</p>
-              </Button>
-            </details>
-            <Button class="w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
-              <p class="text-lg font-bold">便追加</p>
-            </Button>
-          </div>
-        </details>
         <Button class="w-[150px] rounded-full px-4 py-2" color="secondary" variant="raised">
           <p class="text-lg font-bold">路線追加</p>
         </Button>
       </div>
     </div>
   </div>
-</div>
-
-<style>
-  .top_txt {
-    padding: 0.5em; /*文字周りの余白*/
-    color: #494949; /*文字色*/
-    background: #f5fff6; /*背景色*/
-    border-left: solid 10px #188621; /*左線（実線 太さ 色）*/
-  }
-  .accordion {
-    color: #494949; /*文字色*/
-    background: hwb(125 96% 0%); /*背景色*/
-  }
-</style>
+{/await}
